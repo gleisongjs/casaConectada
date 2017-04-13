20d8cf38897979915c1ea5a7550869df8dfc3b77
gleisongjs <gleisonans@hotmail.com>
2017-04-12 22:40:46 -0300

    db funcionando e listando

---
+++ casaConectada/src/java/com/casaconectada/controller/sistemaLaylaDB.java
@@ -0,0 +1,68 @@
+
+package com.casaconectada.controller;
+
+import com.casaconectada.connection.ConnectionFactory;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author RuWindows
+ */
+public class sistemaLaylaDB extends HttpServlet {
+   
+   
+@Override    
+protected void service(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String action = request.getParameter("action");
+        
+        if (action.equals ("layladb")){
+        Connection conn = ConnectionFactory.getConnection();
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String res="";
+        
+        String sql = "SELECT * FROM sensor ORDER BY id;";
+        
+       try {
+           ps = conn.prepareStatement(sql);
+           
+           rs = ps.executeQuery();
+           while(rs.next()){
+               res += "ID: " + rs.getInt("id");
+               res += "<br/>Distância: "+rs.getString("distancia");
+               res += "<br/>Tempo Atual: "+rs.getString("tempoatual");
+               res += "<br/>Quantidade: "+rs.getString("cont");
+               res += "<hr/>";
+           }
+           
+           
+           
+       } catch (SQLException ex) {
+           Logger.getLogger(sistemaLaylaDB.class.getName()).log(Level.SEVERE, null, ex);
+           res = "Erro ao listar o sensor!";
+       }finally{
+           ConnectionFactory.closeConnection(conn, ps, rs);
+       }
+       
+       request.setAttribute("resultado", res);
+       request.getRequestDispatcher("laylaDb.jsp").forward(request, response);
+        
+    }
+}
+
+        }
+    
